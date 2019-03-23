836ce01b81f32d0e08a27765b03eee2f2f1f062e
Victor Rudnev <Victor.Rudnev@risqitlab.com>
2019-03-24 02:48:44 +0300

    Add comments, fix a bug in the second stage, where lg is used for the blue channel instead of lb

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func check(e error) {
 	}
 }
 
+// force x to stay in [0, b) range. x is assumed to be in [-b,2*b) range
 func wrap(x, b int) int {
 	if x < 0 {
 		return x + b
@@ -29,17 +30,20 @@ func wrap(x, b int) int {
 	return x
 }
 
-func offset(m float64) int {
-	sample := seededRand.NormFloat64() * m
+// get normally distributed (rounded to int) value with the specified std. dev.
+func offset(stddev float64) int {
+	sample := seededRand.NormFloat64() * stddev
 	return int(sample)
 }
 
+// brighten the color safely, i.e., by simultaneously reducing contrast
 func brighten(r uint8, add uint8) uint8 {
 	r32, add32 := uint32(r), uint32(add)
 	return uint8(r32 - r32*add32/255 + add32)
 }
 
 func main() {
+	// command line parsing
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [options] [input] [output] \n", os.Args[0])
 		flag.PrintDefaults()
@@ -69,12 +73,17 @@ func main() {
 		seededRand = rand.New(rand.NewSource(*seedPtr))
 	}
 
+	// flag.Args() contain all non-option arguments, i.e., our input and output files
 	reader, err := os.Open(flag.Args()[0])
 	check(err)
 	m, err := png.Decode(reader)
+
+	// trying to obtain raw pointers to color data, since .At(), .Set() are very slow
 	m_raw_stride, m_raw_pix := 0, []uint8(nil)
 
 	switch m.(type) {
+	default:
+		log.Fatal("unknown image type")
 	case *image.NRGBA:
 		m_raw := m.(*image.NRGBA)
 		m_raw_stride = m_raw.Stride
@@ -100,14 +109,21 @@ func main() {
 	STRIDE_MAG := *strideMagPtr
 	for y := 0; y < b.Max.Y; y++ {
 		for x := 0; x < b.Max.X; x++ {
+			// Every BHEIGHT lines in average a new distorted block begins
 			if seededRand.Intn(BHEIGHT*b.Max.X) == 0 {
 				line_off = offset(BOFFSET)
 				stride = seededRand.NormFloat64() * STRIDE_MAG
 				yset = y
 			}
+			// at the line where the block has begun, we don't want to offset the image
+			// so stride_off is 0 on the block's line
 			stride_off := int(stride * float64(y-yset))
+
+			// offset is composed of the blur, block offset, and skew offset (stride)
 			offx := offset(MAG) + line_off + stride_off
 			offy := offset(MAG)
+
+			// copy the corresponding pixel (4 bytes) to the new image
 			src_idx := m_raw_stride*wrap(y+offy, b.Max.Y) + 4*wrap(x+offx, b.Max.X)
 			dst_idx := new_img.Stride*y + 4*x
 
@@ -129,18 +145,21 @@ func main() {
 			lb += seededRand.NormFloat64() * LAG
 			offx := offset(STD_OFFSET)
 
+			// obtain source pixel base offsets. red/blue border is also smoothed by offx
 			ra_idx := new_img.Stride*y + 4*wrap(x+int(lr)-offx, b.Max.X)
 			g_idx := new_img.Stride*y + 4*wrap(x+int(lg), b.Max.X)
-			b_idx := new_img.Stride*y + 4*wrap(x+int(lg)+offx, b.Max.X)
+			b_idx := new_img.Stride*y + 4*wrap(x+int(lb)+offx, b.Max.X)
 
+			// pixels are stored in (r, g, b, a) order in memory
 			r := new_img.Pix[ra_idx]
 			a := new_img.Pix[ra_idx+3]
 			g := new_img.Pix[g_idx+1]
 			b := new_img.Pix[b_idx+2]
 
 			r, g, b = brighten(r, ADD), brighten(g, ADD), brighten(b, ADD)
-			dst_idx := new_img1.Stride*y + 4*x
 
+			// copy the corresponding pixel (4 bytes) to the new image
+			dst_idx := new_img1.Stride*y + 4*x
 			copy(new_img1.Pix[dst_idx:dst_idx+4], []uint8{r, g, b, a})
 		}
 	}
@@ -153,20 +172,24 @@ func main() {
 		for x := 0; x < b.Max.X; x++ {
 			offx := MEAN_ABBER + offset(STD_ABBER) // lower offset arg = longer trails
 
+			// obtain source pixel base offsets. only red and blue are distorted
 			ra_idx := new_img1.Stride*y + 4*wrap(x+offx, b.Max.X)
-			g_idx := new_img1.Stride*y + 4*wrap(x, b.Max.X)
+			g_idx := new_img1.Stride*y + 4*x
 			b_idx := new_img1.Stride*y + 4*wrap(x-offx, b.Max.X)
 
+			// pixels are stored in (r, g, b, a) order in memory
 			r := new_img1.Pix[ra_idx]
 			a := new_img1.Pix[ra_idx+3]
 			g := new_img1.Pix[g_idx+1]
 			b := new_img1.Pix[b_idx+2]
 
+			// copy the corresponding pixel (4 bytes) to the SAME image. this gets us nice colorful trails
 			dst_idx := new_img1.Stride*y + 4*x
 			copy(new_img1.Pix[dst_idx:dst_idx+4], []uint8{r, g, b, a})
 		}
 	}
 
+	// write the image
 	writer, err := os.Create(flag.Args()[1])
 	check(err)
 	png.Encode(writer, new_img1)
